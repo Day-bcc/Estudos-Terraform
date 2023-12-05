9291e650a8a90878bed5a2a5052f846e576b961d
Dayane Costa <78270422+Day-bcc@users.noreply.github.com>
2023-12-05 17:45:35 -0300

    Add files via upload

---
+++ DESAFIO3 - Oficial/variable.tf
@@ -0,0 +1,13 @@
+
+variable "resource_group_name" {
+ type    = string
+  default = "DESAFIO03"
+}
+variable "resource_group_location" {
+ description = "EastUS"
+ type        = string
+}
+variable "vnet_name" {
+ description = "Vnet-desafio3"
+ type        = string
+}
